f0ba2c1fcf0dc138ca75716fa4d68a446ee8602b
SmallNic <nas231@nyu.edu>
2016-02-16 18:07:28 -0500

    added videos to home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require popcorn
+//= require popcorn.capture
 //= require_tree .
 
 $(document).ready(function(){
@@ -27,6 +29,31 @@ $(document).ready(function(){
     console.log("close button")
     $(".dami_logo").css("top","-74px")
   })
+
+  // This block of code must be run _after_ the DOM is ready
+  // This will capture the frame at the 10th second and create a poster
+  // var $BillVonHoene = Popcorn( "#BillVonHoene" ), poster1;
+  // var $JohnRogersJr = Popcorn( "#JohnRogersJr" ), poster2;
+  // var $KeithMestrich = Popcorn( "#KeithMestrich" ), poster3;
+  // var $RobertGreen = Popcorn( "#RobertGreen" ), poster4;
+  // var $RobertRaben = Popcorn( "#RobertRaben" ), poster5;
+
+
+  // Jump to the frame we want to capture and create a poster!
+  // poster1 = $BillVonHoene.currentTime( 10 ).capture();
+  // poster2 = $JohnRogersJr.currentTime( 10 ).capture();
+  // poster3 = $KeithMestrich.currentTime( 10 ).capture();
+  // poster4 = $RobertGreen.currentTime( 10 ).capture();
+  // poster5 = $RobertRaben.currentTime( 10 ).capture();
+
+  // $BillVonHoene.capture({ at:10 });
+  // $JohnRogersJr.capture({ at:10 });
+
+  // $BillVonHoene.listen( "canplayall", function(){
+  //   this.currentTime(10).capture();
+  // })
+
+
 })
 
 $(document).on('page:load', function(){
